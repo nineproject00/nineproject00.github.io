57a85f10e2e2410b7e4996d356f835b148e72c05
frizkaadep <frizkaadep@gmail.com>
2022-09-24 17:35:01 +0700

    add contextmenu
    add contextmenu right click

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,51 +1,44 @@
-/* Template: Tivo - SaaS App HTML Landing Page Template
-   Author: Inovatik
-   Created: Sep 2019
-   Description: Custom JS file
-*/
+(function ($) {
+    "use strict";
 
-
-(function($) {
-    "use strict"; 
-	
-	/* Preloader */
-	$(window).on('load', function() {
-		var preloaderFadeOutTime = 500;
-		function hidePreloader() {
-			var preloader = $('.spinner-wrapper');
-			setTimeout(function() {
-				preloader.fadeOut(preloaderFadeOutTime);
-			}, 500);
-		}
-		hidePreloader();
-	});
-
-	
-	/* Navbar Scripts */
-	// jQuery to collapse the navbar on scroll
-    $(window).on('scroll load', function() {
-		if ($(".navbar").offset().top > 60) {
-			$(".fixed-top").addClass("top-nav-collapse");
-		} else {
-			$(".fixed-top").removeClass("top-nav-collapse");
-		}
+    /* Preloader */
+    $(window).on('load', function () {
+        var preloaderFadeOutTime = 500;
+        function hidePreloader() {
+            var preloader = $('.spinner-wrapper');
+            setTimeout(function () {
+                preloader.fadeOut(preloaderFadeOutTime);
+            }, 500);
+        }
+        hidePreloader();
     });
 
-	// jQuery for page scrolling feature - requires jQuery Easing plugin
-	$(function() {
-		$(document).on('click', 'a.page-scroll', function(event) {
-			var $anchor = $(this);
-			$('html, body').stop().animate({
-				scrollTop: $($anchor.attr('href')).offset().top
-			}, 600, 'easeInOutExpo');
-			event.preventDefault();
-		});
-	});
+
+    /* Navbar Scripts */
+    // jQuery to collapse the navbar on scroll
+    $(window).on('scroll load', function () {
+        if ($(".navbar").offset().top > 60) {
+            $(".fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".fixed-top").removeClass("top-nav-collapse");
+        }
+    });
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(function () {
+        $(document).on('click', 'a.page-scroll', function (event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 600, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    });
 
     // closes the responsive menu on menu item click
-    $(".navbar-nav li a").on("click", function(event) {
-    if (!$(this).parent().hasClass('dropdown'))
-        $(".navbar-collapse").collapse('hide');
+    $(".navbar-nav li a").on("click", function (event) {
+        if (!$(this).parent().hasClass('dropdown'))
+            $(".navbar-collapse").collapse('hide');
     });
 
 
@@ -54,11 +47,11 @@
         autoplay: {
             delay: 2000,
             disableOnInteraction: false
-		},
+        },
         loop: true,
         spaceBetween: 30,
         slidesPerView: 5,
-		breakpoints: {
+        breakpoints: {
             // when window is <= 580px
             580: {
                 slidesPerView: 1,
@@ -85,16 +78,16 @@
 
 
     /* Text Slider - Swiper */
-	var textSlider = new Swiper('.text-slider', {
+    var textSlider = new Swiper('.text-slider', {
         autoplay: {
             delay: 6000,
             disableOnInteraction: false
-		},
+        },
         loop: true,
         navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev'
-		}
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        }
     });
 
 
@@ -109,19 +102,19 @@
         iframe: {
             patterns: {
                 youtube: {
-                    index: 'youtube.com/', 
-                    id: function(url) {        
+                    index: 'youtube.com/',
+                    id: function (url) {
                         var m = url.match(/[\\?\\&]v=([^\\?\\&]+)/);
-                        if ( !m || !m[1] ) return null;
+                        if (!m || !m[1]) return null;
                         return m[1];
                     },
                     src: 'https://www.youtube.com/embed/%id%?autoplay=1'
                 },
                 vimeo: {
-                    index: 'vimeo.com/', 
-                    id: function(url) {        
+                    index: 'vimeo.com/',
+                    id: function (url) {
                         var m = url.match(/(https?:\/\/)?(www.)?(player.)?vimeo.com\/([a-z]*\/)*([0-9]{6,11})[?]?.*/);
-                        if ( !m || !m[5] ) return null;
+                        if (!m || !m[5]) return null;
                         return m[5];
                     },
                     src: 'https://player.vimeo.com/video/%id%?autoplay=1'
@@ -132,33 +125,33 @@
 
 
     /* Details Lightbox - Magnific Popup */
-	$('.popup-with-move-anim').magnificPopup({
-		type: 'inline',
-		fixedContentPos: false, /* keep it false to avoid html tag shift with margin-right: 17px */
-		fixedBgPos: true,
-		overflowY: 'auto',
-		closeBtnInside: true,
-		preloader: false,
-		midClick: true,
-		removalDelay: 300,
-		mainClass: 'my-mfp-slide-bottom'
-	});
-    
-    
+    $('.popup-with-move-anim').magnificPopup({
+        type: 'inline',
+        fixedContentPos: false, /* keep it false to avoid html tag shift with margin-right: 17px */
+        fixedBgPos: true,
+        overflowY: 'auto',
+        closeBtnInside: true,
+        preloader: false,
+        midClick: true,
+        removalDelay: 300,
+        mainClass: 'my-mfp-slide-bottom'
+    });
+
+
     /* Move Form Fields Label When User Types */
     // for input and textarea fields
-    $("input, textarea").keyup(function(){
-		if ($(this).val() != '') {
-			$(this).addClass('notEmpty');
-		} else {
-			$(this).removeClass('notEmpty');
-		}
+    $("input, textarea").keyup(function () {
+        if ($(this).val() != '') {
+            $(this).addClass('notEmpty');
+        } else {
+            $(this).removeClass('notEmpty');
+        }
     });
 
 
     /* Sign Up Form */
-    $("#signUpForm").validator().on("submit", function(event) {
-    	if (event.isDefaultPrevented()) {
+    $("#signUpForm").validator().on("submit", function (event) {
+        if (event.isDefaultPrevented()) {
             // handle the invalid form...
             sformError();
             ssubmitMSG(false, "Please fill all fields!");
@@ -171,16 +164,16 @@
 
     function ssubmitForm() {
         // initiate variables with form content
-		var email = $("#semail").val();
-		var name = $("#sname").val();
-		var password = $("#spassword").val();
+        var email = $("#semail").val();
+        var name = $("#sname").val();
+        var password = $("#spassword").val();
         var terms = $("#sterms").val();
-        
+
         $.ajax({
             type: "POST",
             url: "php/signupform-process.php",
-            data: "email=" + email + "&name=" + name + "&password=" + password + "&terms=" + terms, 
-            success: function(text) {
+            data: "email=" + email + "&name=" + name + "&password=" + password + "&terms=" + terms,
+            success: function (text) {
                 if (text == "success") {
                     sformSuccess();
                 } else {
@@ -189,7 +182,7 @@
                 }
             }
         });
-	}
+    }
 
     function sformSuccess() {
         $("#signUpForm")[0].reset();
@@ -198,10 +191,10 @@
     }
 
     function sformError() {
-        $("#signUpForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+        $("#signUpForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
             $(this).removeClass();
         });
-	}
+    }
 
     function ssubmitMSG(valid, msg) {
         if (valid) {
@@ -214,8 +207,8 @@
 
 
     /* Log In Form */
-    $("#logInForm").validator().on("submit", function(event) {
-    	if (event.isDefaultPrevented()) {
+    $("#logInForm").validator().on("submit", function (event) {
+        if (event.isDefaultPrevented()) {
             // handle the invalid form...
             lformError();
             lsubmitMSG(false, "Please fill all fields!");
@@ -228,14 +221,14 @@
 
     function lsubmitForm() {
         // initiate variables with form content
-		var email = $("#lemail").val();
-		var password = $("#lpassword").val();
-        
+        var email = $("#lemail").val();
+        var password = $("#lpassword").val();
+
         $.ajax({
             type: "POST",
             url: "php/loginform-process.php",
-            data: "email=" + email + "&password=" + password, 
-            success: function(text) {
+            data: "email=" + email + "&password=" + password,
+            success: function (text) {
                 if (text == "success") {
                     lformSuccess();
                 } else {
@@ -244,7 +237,7 @@
                 }
             }
         });
-	}
+    }
 
     function lformSuccess() {
         $("#logInForm")[0].reset();
@@ -253,10 +246,10 @@
     }
 
     function lformError() {
-        $("#logInForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+        $("#logInForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
             $(this).removeClass();
         });
-	}
+    }
 
     function lsubmitMSG(valid, msg) {
         if (valid) {
@@ -269,8 +262,8 @@
 
 
     /* Newsletter Form */
-    $("#newsletterForm").validator().on("submit", function(event) {
-    	if (event.isDefaultPrevented()) {
+    $("#newsletterForm").validator().on("submit", function (event) {
+        if (event.isDefaultPrevented()) {
             // handle the invalid form...
             nformError();
             nsubmitMSG(false, "Please fill all fields!");
@@ -283,13 +276,13 @@
 
     function nsubmitForm() {
         // initiate variables with form content
-		var email = $("#nemail").val();
+        var email = $("#nemail").val();
         var terms = $("#nterms").val();
         $.ajax({
             type: "POST",
             url: "php/newsletterform-process.php",
-            data: "email=" + email + "&terms=" + terms, 
-            success: function(text) {
+            data: "email=" + email + "&terms=" + terms,
+            success: function (text) {
                 if (text == "success") {
                     nformSuccess();
                 } else {
@@ -298,7 +291,7 @@
                 }
             }
         });
-	}
+    }
 
     function nformSuccess() {
         $("#newsletterForm")[0].reset();
@@ -307,10 +300,10 @@
     }
 
     function nformError() {
-        $("#newsletterForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+        $("#newsletterForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
             $(this).removeClass();
         });
-	}
+    }
 
     function nsubmitMSG(valid, msg) {
         if (valid) {
@@ -320,11 +313,11 @@
         }
         $("#nmsgSubmit").removeClass().addClass(msgClasses).text(msg);
     }
-    
+
 
     /* Privacy Form */
-    $("#privacyForm").validator().on("submit", function(event) {
-    	if (event.isDefaultPrevented()) {
+    $("#privacyForm").validator().on("submit", function (event) {
+        if (event.isDefaultPrevented()) {
             // handle the invalid form...
             pformError();
             psubmitMSG(false, "Please fill all fields!");
@@ -337,16 +330,16 @@
 
     function psubmitForm() {
         // initiate variables with form content
-		var name = $("#pname").val();
-		var email = $("#pemail").val();
+        var name = $("#pname").val();
+        var email = $("#pemail").val();
         var select = $("#pselect").val();
         var terms = $("#pterms").val();
-        
+
         $.ajax({
             type: "POST",
             url: "php/privacyform-process.php",
-            data: "name=" + name + "&email=" + email + "&select=" + select + "&terms=" + terms, 
-            success: function(text) {
+            data: "name=" + name + "&email=" + email + "&select=" + select + "&terms=" + terms,
+            success: function (text) {
                 if (text == "success") {
                     pformSuccess();
                 } else {
@@ -355,7 +348,7 @@
                 }
             }
         });
-	}
+    }
 
     function pformSuccess() {
         $("#privacyForm")[0].reset();
@@ -364,10 +357,10 @@
     }
 
     function pformError() {
-        $("#privacyForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+        $("#privacyForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
             $(this).removeClass();
         });
-	}
+    }
 
     function psubmitMSG(valid, msg) {
         if (valid) {
@@ -377,13 +370,13 @@
         }
         $("#pmsgSubmit").removeClass().addClass(msgClasses).text(msg);
     }
-    
+
 
     /* Back To Top Button */
     // create the back to top button
     $('body').prepend('<a href="body" class="back-to-top page-scroll">Back to Top</a>');
     var amountScrolled = 700;
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         if ($(window).scrollTop() > amountScrolled) {
             $('a.back-to-top').fadeIn('500');
         } else {
@@ -392,9 +385,9 @@
     });
 
 
-	/* Removes Long Focus On Buttons */
-	$(".button, a, button").mouseup(function() {
-		$(this).blur();
-	});
+    /* Removes Long Focus On Buttons */
+    $(".button, a, button").mouseup(function () {
+        $(this).blur();
+    });
 
 })(jQuery);
